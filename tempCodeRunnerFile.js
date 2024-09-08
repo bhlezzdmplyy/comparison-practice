dc4c2307c19e60869d0b8f255edc0157d6178766
Bhlezzdmplyyyy <yharrasallyguiao>
2024-09-09 00:16:48 +0800

    initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 11;
+
+//Use comparison operators and store the results is variables
+let isXGreaterThanY = x > y;
+let isXlessThanY = x < y;
+let isXEqualToY = x == y;
+let isXNotEqualToY = x !== y;
+
+console.log("Is X greater than Y?", isXGreaterThanY);
+console.log("is X less than Y?", isXlessThanY);
+console.log("is X equal to Y?", isXEqualToY);
+console.log("is X not equal to Y?", isXNotEqualToY);
